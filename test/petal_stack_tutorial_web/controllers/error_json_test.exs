afce5f52bb3555b7fd5337218b451b01f55f63da
Tyler Alterio <taalteri@amazon.com>
2024-06-02 20:59:06 -0400

    mix format

--- test/petal_stack_tutorial_web/controllers/error_json_test.exs
+++ test/petal_stack_tutorial_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule PetalStackTutorialWeb.ErrorJSONTest do
   use PetalStackTutorialWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert PetalStackTutorialWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert PetalStackTutorialWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
